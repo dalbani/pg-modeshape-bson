d9cc43f2a2a12ce6cbe31059fb8b65a60ecd7b4b
Damiano Albani <damiano.albani@gmail.com>
2018-01-25 13:15:48 +0100

    Add definition of "modeshape_bson_version()" in .control file

--- modeshape_bson--1.0.sql
+++ modeshape_bson--1.0.sql
@@ -1,6 +1,12 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION modeshape_bson" to load this file. \quit
 
+CREATE FUNCTION modeshape_bson_version()
+    RETURNS text
+    AS 'MODULE_PATHNAME'
+    LANGUAGE C STRICT IMMUTABLE;
+
+
 CREATE FUNCTION json_text_to_modeshape_bson(text)
     RETURNS bytea
     AS 'MODULE_PATHNAME'
